79cb7f8bb8183309a84a382ec6ba6899d28e35c8
Muhammad Faique <muhammad.faique@camp1.tkxel.com>
2026-05-18 17:44:34 +0500

    change-in-arch

--- src/app/core/services/file-reader.service.ts
+++ src/app/core/services/file-reader.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { FileAttachmentDto } from '../models/file-attachment.dto';
+import { FileAttachmentDto } from '../../shared/models/file-attachment.dto';
 
 @Injectable({ providedIn: 'root' })
 export class FileReaderService {
